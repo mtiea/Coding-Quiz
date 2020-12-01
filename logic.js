d49e44593373da09794d72536a60a534962d9ecd
Magdi Tiea <magditiea@gmail.com>
2020-12-01 19:54:57 -0400

    question, choices and answer array created

--- logic.js
+++ logic.js
@@ -1,1 +1,5 @@
+// variables to keep track of quiz state
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
 
--- questions.js
+++ questions.js
@@ -1,1 +1,33 @@
+// array of questions, choices and answers
 
+var questions = [
+    {
+      title: "What event do you want HTML to listen for when you left click your mouse on an object?",
+      choices: [ "keydown", "cut", "click", "drag" ],
+      answer: "click"
+    },
+
+{    
+     title: "The _____ node type represents the entire page",
+     choices: [ "DocumentType", "Text", "Entity", "Document" ],
+     answers: "Document"
+},
+
+{
+     title: "The ____ function rounds a number up",
+     choices: [ "math.random", "math.ceil", "math.up", "math.down" ],
+     answers: "math.ceil"
+},
+
+{
+    title: "Which syntax can be used for a numer or a string",
+    choices: [ "===", "==", "=", "=>" ],
+    answers: "==="
+},
+
+{
+    title: "____ is used next to <script to link a Javascript file in HTML",
+    choices: [ "rel", "href", "link", "src" ],
+    answers: "src"
+}
+]
