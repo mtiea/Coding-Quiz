cc61893b4aff01ebd13533198278e65f7e60a8b3
Magdi Tiea <magditiea@gmail.com>
2020-12-04 14:57:59 -0400

    sound effects added

--- logic.js
+++ logic.js
@@ -12,6 +12,10 @@ var choices1 = document.getElementById("choices");
 var timer1 = document.getElementById("timer");
 var initials1 = document.getElementById("initials");
 
+// sound effects
+var sfxRight = new Audio("assets/sfx/correct.wav");
+var sfxWrong = new Audio("assets/sfx/incorrect.wav");
+
 
 // function to start the quiz
 function quizStart() {
@@ -45,7 +49,7 @@ function grabquestion() {
 
     // update title with the current question
     var title1 = document.getElementById("question-title");
-    title1.textContent = currentquestion.title;
+    title1.textContent = currentquestion.title; 
 
     // clear out any old question choices
     choices1.innerHTML = "";
@@ -67,4 +71,65 @@ function grabquestion() {
     choicesEl.appendChild(choiceNode);
 });
 }
+//function for clicking the question
+function questionClick() {
+// checking if the user guessed wrong
+ if (this.value !== questions[currentQuestionIndex.anser]) {
+    // take off 15 seconds
+    time -= 15;
+    if(time < 0) {
+        time = 0;
+    }
+ // display the new time on the page
+ timer1.textContent = time;
 
+ // play wrong sound effect
+ sfxWrong.play();
+
+ feedback1.textContent = "Wrong!";
+ } else {
+  // play "right" sound effect
+  sfxRight.play();
+
+  feedback1.textContent = "Correct!";
+ } 
+ // flash right/wrong feedback on page for half a second
+ feedback1.setAttribute("class", "feedback");
+ setTimeout(function() {
+   feedback1.setAttribute("class", "feedback hide");
+ }, 1000);
+
+ // move to the next question
+ currentQuestionIndex++;
+
+ // check if we have ran out of questions
+  if (currentQuestionIndex === questions.length ) {
+      endQuiz()
+  } else {
+      grabquestion()
+  }
+}
+  // function for ending quiz
+  function endQuiz() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
