3468c1c6173b7b3b368433d189992e77354b5c24
Magdi Tiea <magditiea@gmail.com>
2020-12-04 02:11:04 -0400

    function for quiz start and timer created

--- logic.js
+++ logic.js
@@ -3,3 +3,33 @@ var currentQuestionIndex = 0;
 var time = questions.length * 15;
 var timerId;
 
+// variables to link DOM elements
+var questions1 = document.getElementById("questions");
+var sumbitBtn = document.getElementById("submit");
+var feedback1 = document.getElementById("feedback");
+var startBtn = document.getElementById("start");
+var choices1 = document.getElementById("choices");
+var timer1 = document.getElementById("timer");
+var initials1 = document.getElementById("initials");
+
+
+// function to start the quiz
+function quizStart() {
+
+ //hide start screen
+ var startScreen  = document.getElementById("start-screen");
+ startScreen.setAttribute("class", "hide");
+}
+// un-hide 
+questions1.remove.Attribute("class");
+
+//start timer
+timerid = setInterval(clocktick, 1000);
+
+// function to start the clock
+function setInterval() {
+    setInterval(setalert, 6000);
+}
+
+// show starting time
+timer1.textContent = time;
